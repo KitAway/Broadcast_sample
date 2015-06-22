371b0c43e807cb005fd33018708718df539d9de3
KitAway <liang.ma@studenti.polito.it>
2015-06-22 16:52:54 +0200

    receiver activity implemented

--- app/src/main/java/com/example/d038395/broadcast_sample/MainActivity.java
+++ app/src/main/java/com/example/d038395/broadcast_sample/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.d038395.broadcast_sample;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -34,5 +36,11 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void broadcastMessage(View view) {
+        Intent intent =new Intent();
+        intent.setAction("Liang_INTENT");
+        sendBroadcast(intent);
     }
 }
---
+++ app/src/main/java/com/example/d038395/broadcast_sample/MyReceiver.java
@@ -0,0 +1,19 @@
+package com.example.d038395.broadcast_sample;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class MyReceiver extends BroadcastReceiver {
+    public MyReceiver() {
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // TODO: This method is called when the BroadcastReceiver is receiving
+        // an Intent broadcast.
+        Toast.makeText(context,"Intend Detected.",Toast.LENGTH_SHORT).show();
+        //throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
